b5b60f36c9891be0de6a945e196eec7867204174
Daniel Ellis <ellisd23@gmail.com>
2016-05-18 12:19:59 -0700

    Move memcache command parsing into own file.

--- main.go
+++ main.go
@@ -1,78 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"io/ioutil"
-	"strings"
 	"time"
 )
 
-const (
-	ERR_NONE = iota
-	ERR_NO_CMD
-	ERR_INVALID_CMD
-	ERR_TRUNCATED_CMD
-	ERR_INCOMPLETE_CMD
-)
-
 const CAPTURE_SIZE = 9000
-
-var VALID_READ_CMDS = []string{"get", "gets"}
-var VALID_WRITE_CMDS = []string{"set", "add", "replace", "append", "prepend"}
-
-func itemInArray(item string, array []string) bool {
-	for _, i := range array {
-		if i == item {
-			return true
-		}
-	}
-	return false
-}
-
-// parseCommand parses a command and list of keys the command is operating on from
-// a sequence of application data bytes.
-func parseCommand(app_data []byte) (cmd string, keys []string, cmd_err int) {
-
-	// Parse out the command
-	space_i := bytes.IndexByte(app_data, byte(' '))
-	if space_i == -1 {
-		return "", []string{}, ERR_NO_CMD
-	}
-
-	// Find the first newline
-	newline_i := bytes.Index(app_data, []byte("\r\n"))
-	if newline_i == -1 {
-		return "", []string{}, ERR_TRUNCATED_CMD
-	}
-
-	// Validate command
-	split_data := strings.Split(string(app_data[:newline_i]), " ")
-	is_read_cmd := itemInArray(split_data[0], VALID_READ_CMDS)
-	is_write_cmd := itemInArray(split_data[0], VALID_WRITE_CMDS)
-	if !is_read_cmd && !is_write_cmd {
-		return "", []string{}, ERR_INVALID_CMD
-	}
-
-	// Extract command & keys
-	if is_read_cmd {
-		// get commands can be multiple keys
-		cmd, keys = split_data[0], split_data[1:]
-	} else {
-		// set commands can only be one key
-		cmd, keys = split_data[0], split_data[1:2]
-	}
-
-	// Validate keys
-	if len(keys) == 0 || (len(keys) == 1 && keys[0] == "") {
-		return "", []string{}, ERR_INCOMPLETE_CMD
-	}
-
-	return cmd, keys, ERR_NONE
-}
 
 // startReportingLoop starts a loop that will periodically output statistics
 // on the hottest keys, and optionally, errors that occured in parsing.
